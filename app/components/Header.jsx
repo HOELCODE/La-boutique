43c241f4543cda03cbc66c83fa0c18637ac5a33f
HOELCODE <hoelpro@outlook.fr>
2024-01-21 21:21:51 +0100

    test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -50,7 +50,7 @@ export function HeaderMenu({menu, primaryDomainUrl, viewport}) {
           style={activeLinkStyle}
           to="/"
         >
-          Maison
+          Home
         </NavLink>
       )}
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
